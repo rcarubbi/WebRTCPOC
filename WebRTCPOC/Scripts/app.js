1e9721abc6e45c577e0986e8d59fe36d6a1a67ed
Raphael Carubbi Neto <rcarubbi@gmail.com>
2017-07-06 19:25:52 -0300

    alteração do método de gravação para Janus WebRTC Gateway

--- WebRTCPOC/Scripts/app.js
+++ WebRTCPOC/Scripts/app.js
@@ -1,8 +1,5 @@
 
 
-// stop them later
-// heartbeater.stop();
-
 function makePeerHeartbeater(peer) {
     var timeoutId = 0;
     function heartbeat() {
@@ -25,11 +22,8 @@ function makePeerHeartbeater(peer) {
     };
 }
 var userInfoAPI = root + "/api/User";
-var localAudioRec,
-    localVideoRec,
-    localStream;
+var localStream;
 var heartbeater;
-var audioContext = new AudioContext;
 
 navigator.getWebcam = (navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
@@ -62,27 +56,47 @@ peer.on('open', function () {
 peer.on('call', function (call) {
     // Answer automatically for demo
     call.answer(window.localStream);
-
+    startRecording();
     step3(call);
 });
+
+
+var server = null;
+if (window.location.protocol === 'http:')
+    server = "http://" + window.location.hostname + ":8088/janus";
+else
+    server = "https://" + window.location.hostname + ":8089/janus";
+
+var janus = null;
+var recordplay = null;
+var bandwidth = 1024 * 1024;
+
+
+var myname = null;
+var recording = false;
+var playing = false;
+var recordingId = null;
+var selectedRecording = null;
+var selectedRecordingInfo = null;
 
 // Click handlers setup
 $(function () {
 
-
+   
 
     $('#make-call').click(function () {
         //Initiate a call!
         $.get(userInfoAPI, { name: $('#callto-id').val(), id: "" }, function (id) {
             var call = peer.call(id, window.localStream);
             step3(call);
+            startRecording();
         });
     });
     $('#end-call').click(function (e) {
         e.preventDefault();
         if (window.existingCall)
             window.existingCall.close();
-        stopRecord();
+        stop();
         step2();
     });
 
@@ -102,6 +116,19 @@ function step1() {
         // Display the video stream in the video object
         $('#my-video').prop('src', URL.createObjectURL(stream));
 
+        Janus.init({
+            debug: "all",
+            callback: function () {
+                janus = new Janus(
+                    {
+                        server: server,
+                        success: janusSuccess,
+                        error: janusError,
+                        destroyed: janusDestroyed
+                    });
+            }
+        });
+
         window.localStream = stream;
         step2();
     }, function () { $('#step1-error').show(); });
@@ -116,12 +143,11 @@ function step3(call) {
     // Hang up on an existing call if present
     if (window.existingCall) {
         window.existingCall.close();
-        stopRecord();
+        stop();
     }
 
     // Wait for stream on the call, then setup peer video
     call.on('stream', function (stream) {
-        beginRecord(call.peer);
         $('#their-video').prop('src', URL.createObjectURL(stream));
     });
 
@@ -130,30 +156,216 @@ function step3(call) {
         $('.their-id').text(name);
     });
 
-    call.on('close', function () { step2(); stopRecord(); });
+    call.on('close', function () {
+        stop();
+        step2();
+    });
+
     $('#step1, #step2').hide();
     $('#step3').show();
 }
 
-var videoRecordTask, audioRecordTask;
-function beginRecord(id) {
-    var videoWebsocketUri = "wss://" + window.location.hostname + ":" + (window.location.port || "443") + "/api/WebRTCVideoRecord";
-    localVideoRec = new WSVideoRecorder(window.localStream, videoWebsocketUri, id);
 
-    var audioWebsocketUri = "wss://" + window.location.hostname + ":" + (window.location.port || "443") + "/api/WebRTCAudioRecord";
-    var input = audioContext.createMediaStreamSource(window.localStream);
-    localAudioRec = new WSAudioRecorder(input, audioWebsocketUri, id);
+function now() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    var today = '' + yyyy + mm + dd
+    return today;
+}
+// gravação
+
+function startRecording() {
+    if (recording)
+        return;
+    // Start a recording
+    recording = true;
+    playing = false;
+    myname = $('#my-id').text() + now()
+
+    // bitrate and keyframe interval can be set at any time: 
+    // before, after, during recording
+    recordplay.send({
+        'message': {
+            'request': 'configure',
+            'video-bitrate-max': bandwidth, // a quarter megabit
+            'video-keyframe-interval': 15000 // 15 seconds
+        }
+    });
+
+    recordplay.createOffer(
+        {
+            // By default, it's sendrecv for audio and video...
+            success: function (jsep) {
+                Janus.debug("Got SDP!");
+                Janus.debug(jsep);
+                var body = { "request": "record", "name": myname };
+                recordplay.send({ "message": body, "jsep": jsep });
+            },
+            error: function (error) {
+                Janus.error("WebRTC error...", error);
+                recordplay.hangup();
+            }
+        });
+
 }
 
-
-function stopRecord() {
-    if (localVideoRec != undefined)
-        localVideoRec.stop();
-    if (localAudioRec != undefined)
-        localAudioRec.stop();
+function stop() {
+    // Stop a recording/playout
+    var stop = { "request": "stop" };
+    recordplay.send({ "message": stop });
+    recordplay.hangup();
 }
 
+function janusSuccess() {
 
+    // Attach to echo test plugin
+    janus.attach(
+        {
+            plugin: "janus.plugin.recordplay",
+            success: attachPluginRecordPlaySuccess,
+            error: attachPluginRecordPlayError,
+            consentDialog: consentDialog,
+            webrtcState: webrtcState,
+            onmessage: recordPlayOnMessage,
+            onlocalstream: function (stream) {
+                if (playing === true)
+                    return;
+                Janus.debug(" ::: Got a local stream :::");
+                Janus.debug(JSON.stringify(stream));
 
+            },
+            onremotestream: function (stream) {
+                if (playing === false)
+                    return;
+                Janus.debug(" ::: Got a remote stream :::");
+                Janus.debug(JSON.stringify(stream));
+            },
+            oncleanup: function () {
+                Janus.log(" ::: Got a cleanup notification :::");
+                // FIXME Reset status
+                recording = false;
+                playing = false;
+            }
+        });
+}
 
+function janusError(error) {
+    Janus.error(error);
+}
 
+function janusDestroyed() {
+    window.location.reload();
+}
+
+function attachPluginRecordPlaySuccess(pluginHandle) {
+    recordplay = pluginHandle;
+    Janus.log("Plugin attached! (" + recordplay.getPlugin() + ", id=" + recordplay.getId() + ")");
+}
+
+function attachPluginRecordPlayError(error) {
+    Janus.error("  -- Error attaching plugin...", error);
+}
+
+function consentDialog(on) {
+    Janus.debug("Consent dialog should be " + (on ? "on" : "off") + " now");
+}
+
+function webrtcState(on) {
+    Janus.log("Janus says our WebRTC PeerConnection is " + (on ? "up" : "down") + " now");
+}
+
+function recordPlayOnMessage(msg, jsep) {
+    Janus.debug(" ::: Got a message :::");
+    Janus.debug(JSON.stringify(msg));
+    var result = msg["result"];
+    if (result !== null && result !== undefined) {
+        if (result["status"] !== undefined && result["status"] !== null) {
+            var event = result["status"];
+            if (event === 'preparing') {
+                Janus.log("Preparing the recording playout");
+                recordplay.createAnswer(
+                    {
+                        jsep: jsep,
+                        media: { audioSend: false, videoSend: false },	// We want recvonly audio/video
+                        success: recordPlayCreateAnswerSuccess,
+                        error: recordPlayCreateAnswerError
+                    });
+                if (result["warning"])
+                    Janus.log(result["warning"]);
+            } else if (event === 'recording') {
+                // Got an ANSWER to our recording OFFER
+                if (jsep !== null && jsep !== undefined)
+                    recordplay.handleRemoteJsep({ jsep: jsep });
+                var id = result["id"];
+                if (id !== null && id !== undefined) {
+                    Janus.log("The ID of the current recording is " + id);
+                    recordingId = id;
+                }
+            } else if (event === 'slow_link') {
+                var uplink = result["uplink"];
+                if (uplink !== 0) {
+                    // Janus detected issues when receiving our media, let's slow down
+                    bandwidth = parseInt(bandwidth / 1.5);
+                    recordplay.send({
+                        'message': {
+                            'request': 'configure',
+                            'video-bitrate-max': bandwidth, // Reduce the bitrate
+                            'video-keyframe-interval': 15000 // Keep the 15 seconds key frame interval
+                        }
+                    });
+                }
+            } else if (event === 'playing') {
+                Janus.log("Playout has started!");
+            } else if (event === 'stopped') {
+                Janus.log("Session has stopped!");
+                var id = result["id"];
+                if (recordingId !== null && recordingId !== undefined) {
+                    if (recordingId !== id) {
+                        Janus.warn("Not a stop to our recording?");
+                        return;
+                    }
+                }
+                if (selectedRecording !== null && selectedRecording !== undefined) {
+                    if (selectedRecording !== id) {
+                        Janus.warn("Not a stop to our playout?");
+                        return;
+                    }
+                }
+                // FIXME Reset status
+                recordingId = null;
+                recording = false;
+                playing = false;
+                recordplay.hangup();
+            }
+        }
+    } else {
+        // FIXME Error?
+        var error = msg["error"];
+
+        // FIXME Reset status
+        recording = false;
+        playing = false;
+        recordplay.hangup();
+    }
+}
+
+function recordPlayCreateAnswerSuccess(jsep) {
+    Janus.debug("Got SDP!");
+    Janus.debug(jsep);
+    var body = { "request": "start" };
+    recordplay.send({ "message": body, "jsep": jsep });
+}
+
+function recordPlayCreateAnswerError(error) {
+    Janus.error("WebRTC error:", error);
+
+}
